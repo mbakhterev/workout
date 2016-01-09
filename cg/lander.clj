480ccc526c453728db4a18c871a0325863ed4876
M.Bakhterev <mob@k.imm.uran.ru>
2016-01-09 19:01:38 +0500

    Успешная посадка первого уровня lander-1@CG

--- cg/lander.clj
+++ cg/lander.clj
@@ -16,17 +16,34 @@
 
 (def ^:const M 3.711)
 
-(defn- thrust [h dh p]
-  (let [)
+(defn- integrate [[h v] p]
+  (let [a (- M p)] [(+ h v (* 0.5 a a)) (+ v a)]))
+
+(defn- solve-sq [a b c]
+  (let [D (- (* b b) (* 4.0 a c))]
+    (if (< D 0)
+      nil
+      (let [sqrt-D (Math/sqrt D) q (* 2 a)]
+        (mapv (fn [op] (/ (op (- b) sqrt-D) q)) [+ -])))))
+
+(defn- thrust [H V P]
+  (let [[h v] (reduce integrate [H (- V)] (range P 4))
+;        h (- H pre-h)
+        a (- M 4)
+        [tp tm] (solve-sq (* 0.5 a) v (- h))]
+    (binding [*out* *err*]
+      (println "HVP:" [H V P] "hv:" [h v] "times:" [tp tm]))
+    (if (and tp (< 37 (+ v (* a tp)))) 4 0)))
 
 (defn -main [& args]
   (let [surface (partition 2 1 (partition 2 (repeatedly (* 2 (read)) read)))]
     (binding [*out* *err*] (println "Surface:" surface))
-    (while true
-      (let [[X Y x-speed y-speed fuel rotate power :as game] (vec (repeatedly 7 read))
+    (loop [hold 0]
+      (let [[X Y dx dy fuel rotate power :as game] (vec (repeatedly 7 read))
             h (height surface X Y)]
-        (binding [*out* *err*] (println "height:" h))
-        (println "0 3")))))
+        (binding [*out* *err*] (println "height:" h "thurst:" power))
+        (println (str 0 \space (thrust h dy power)))
+      (recur (inc hold))))))
 
 ; Auto-generated code below aims at helping you parse
 ; the standard input according to the problem statement.
