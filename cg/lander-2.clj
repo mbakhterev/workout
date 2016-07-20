3b4fe82433539967a9a33d42cc78708beea423ad
M.Bakhterev <mob@k.imm.uran.ru>
2016-07-20 20:57:07 +0500

    Lander-2: Таблицы для генерации облака возможных движений

--- cg/lander-2.clj
+++ cg/lander-2.clj
@@ -1,44 +1,5 @@
 (ns Player (:gen-class))
 
-(defn- height [surface x y]
-  (let [[xa xb ya yb] (for [i [first second] j [first second]] (comp i j))
-        cut           (first (filter (fn [p] (<= (xa p) x (xb p))) surface))
-        [x1 y1 x2 y2] ((juxt xa ya xb yb) cut)
-        k             (float (/ (- y2 y1) (- x2 x1)))
-        yp            (+ y1 (* k (- x x1)))]
-    (- y yp)))
-
-(def ^:const ^double M 3.711)
-
-(defn- integrate [[h v] p]
-  (let [a (- M p)] [(+ h v (* 0.5 a a)) (+ v a)]))
-
-(defn- solve-sq [a b c]
-  (let [D (- (* b b) (* 4.0 a c))]
-    (if (< D 0)
-      nil
-      (let [sqrt-D (Math/sqrt D) q (* 2 a)]
-        (mapv (fn [op] (/ (op (- b) sqrt-D) q)) [+ -])))))
-
-(defn- thrust [H V P]
-  (let [[h v] (reduce integrate [H (- V)] (range P 4))
-        a (- M 4)
-        [tp tm] (solve-sq (* 0.5 a) v (- h))]
-    (binding [*out* *err*]
-      (println "HVP:" [H V P] "hv:" [h v] "times:" [tp tm]))
-    (if (and tp (< 37 (+ v (* a tp)))) 4 0)))
-
-
-(comment (defn -main [& args]
-  (let [surface (partition 2 1 (partition 2 (repeatedly (* 2 (read)) read)))]
-    (binding [*out* *err*] (println "Surface:" surface))
-    (loop [hold 0]
-      (let [[X Y dx dy fuel rotate power :as game] (vec (repeatedly 7 read))
-            h (height surface X Y)]
-        (binding [*out* *err*] (println "height:" h "thurst:" power)) 
-        (println (str 0 \space (thrust h dy power))) 
-      (recur (inc hold)))))))
-
 (defrecord Point [^double x ^double y])
 (defrecord Section [^Point a ^Point b])
 
@@ -94,15 +55,30 @@
     (->Lander (+ x dx (* 0.5 ddx)) (+ y dy (* 0.5 ddy)) (+ dx ddx) (+ dy ddy)
               (- fuel power) angle power)))
 
-; Экономим на копейках, на всякий случай
+; Экономим на копейках, на всякий случай рассчитываем все ходы заранее. Потому
+; что в runtime придётся довольно много if-ов обрабатывать. А так, просто из
+; таблицы выбор
 
-(defn- ^:const power-cloud (mapv identity (range 0 1)))
+(defn- gen-cloud [base cloud array-convert]
+  (let [A (first base)
+        B (last base)]
+    (->> base
+         (mapv (fn [p] (->> cloud
+                            (map (partial + p))
+                            (filter (fn [i] (<= A i B)))
+                            array-convert
+                            vec))))))
 
-(def ^:const ^long angle-step 5)
-(def ^:const 
+(def ^:const angle-step 5)
+(def ^:const angle-cloud-table (gen-cloud (range -90 91)
+                                          (range -15 (+ 15 angle-step) angle-step)
+                                          long-array))
 
-(def ^:const ^doubles power-cloud [-1.0 0.0 1.0])
+(defn- ^longs angle-cloud [^long phi] (angle-cloud-table (+ 90 phi)))
 
+(def ^:const power-cloud (gen-cloud (range 0 5)
+                                    (range -1.0 2.0)
+                                    double-array))
 
 (defn -main [& args]
   (let [S (read-surface)      
@@ -115,13 +91,14 @@
       ;          R - опасные места, рядом с которыми не летаем
 
 
+    (dump "power-cloud:" power-cloud)
+    (dump "angle-cloud:" (take 5 (drop 5 angle-cloud-table)))
     (dump "surface:" S)
     (dump "game:" G)
     (dump "landings:" L)
     (dump "rocks:" R)
     
     (loop [game G] 
-      ; (dump game)
       (println 81 4)
 
       (let [next-game (read-lander)
