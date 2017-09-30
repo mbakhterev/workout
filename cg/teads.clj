0c1c0738c0b3bb9008819a65e9528744f6368dd7
M.Bakhterev <mob@k.imm.uran.ru>
2017-09-30 21:23:18 +0500

    Teads: работает

--- cg/teads.clj
+++ cg/teads.clj
@@ -8,16 +8,6 @@
 
 (defrecord Edge [^int u ^int v])
 
-(letfn [(normalize [[^long u ^long v :as e]] (if (<= u v) (vec e) [v u]))]
-  (defn- load-graph-1 ^Graph []
-    (let [E (partition 2 (repeatedly (* 2 (read)) read)) ; (seq (set (map normalize edges)))
-          M (reduce (fn ^long [^long M [^long u ^long v]] (max M u v)) 0 E)
-          D (int-array (+ 1 M))]
-      (doseq [[u v] E]
-        (aset D u (+ 1 (aget D u)))
-        (aset D v (+ 1 (aget D v))))
-      (->Graph E D))))
-
 (defn- load-graph-2 ^Graph []
   (let [s (java.util.Scanner. *in*)
         N (.nextInt s)
@@ -28,40 +18,10 @@
                         v (.nextInt s)]
                     (recur (- n 1) (max max-id u v) (conj! edges [u v])))))
         D (int-array (+ 1 M))]
-;    (dump "N:" N "edges:" (count E))
     (doseq [[u v] E]
       (aset D u (+ 1 (aget D u)))
       (aset D v (+ 1 (aget D v))))
     (->Graph E D)))
-
-(defn- load-graph-3 ^Graph []
-  (let [s (java.util.Scanner. *in*)
-        [E M D-map] (loop [N (.nextInt s) max-id 0 edges (transient []) degrees (transient {})]
-                      (if (zero? N)
-                        [(persistent! edges) max-id (persistent! degrees)]
-                        (let [u (.nextInt s)
-                              v (.nextInt s)]
-                          (recur (- N 1)
-                                 (max max-id u v)
-                                 (conj! edges [u v])
-                                 (assoc! degrees
-                                         u (+ 1 (get degrees u 0))
-                                         v (+ 1 (get degrees v 0)))))))
-        D (int-array (+ 1 M))]
-    (doseq [[u N] D-map] (aset D u N))
-    (->Graph E D)))
-
-(defn- filter-leaves ^Graph [^Graph {E :edges D :degrees :as G}]
-  (let [one (fn ^Leaves [^Leaves L [^long u ^long v :as e]]
-              (if (and (< 1 (aget D u))
-                       (< 1 (aget D v)))
-                (assoc L :internals (conj (:internals L) e))
-                (assoc L :leaves (conj (:leaves L) e))))
-        {L :leaves I :internals} (reduce one (->Leaves '() '()) E)]
-    (doseq [[u v] L]
-      (aset D u (- (aget D u) 1))
-      (aset D v (- (aget D v) 1)))
-    (assoc G :edges I)))
 
 (defn- filter-2 ^Graph [^Graph {D :degrees :as G}]
   (let [[E V] (loop [E-graph (:edges G) E-internal (transient []) verticies (transient [])]
@@ -78,12 +38,8 @@
     (assoc G :edges E)))
 
 (defn -main [& args]
-  (let [init-G (time (load-graph-2))]
-    (dump (count (:edges init-G)))
+  (let [init-G (load-graph-2)]
     (loop [G init-G steps 0]
-      (dump steps)
-;      (dump (:edges G))
-;      (dump (vec (:degrees G))) 
       (if (not (empty? (:edges G)))
-        (recur (time (filter-2 G)) (+ 1 steps))
+        (recur (filter-2 G) (+ 1 steps))
         (println steps)))))
