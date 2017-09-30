930c8b081eb0522696507c54b1d84d1c8cefc126
mbakhterev <mob@k.imm.uran.ru>
2017-09-30 23:45:11 +0500

    Teads: немного оптимизации

--- cg/teads.clj
+++ cg/teads.clj
@@ -1,12 +1,10 @@
-(ns teads (:gen-class))
+(ns Solution (:gen-class))
+
+(defmacro ^:private dump-times [code] `(binding [*out* *err*] (time ~code)))
 
 (defn- dump [& args] (binding [*out* *err*] (apply println args)))
 
-(defrecord Graph [edges ^ints degrees])
-
-(defrecord Leaves [leaves internals])
-
-(defrecord Edge [^int u ^int v])
+(defrecord Graph [edges ^ints degrees ^ints notes])
 
 (defn- load-graph-2 ^Graph []
   (let [s (java.util.Scanner. *in*)
@@ -19,11 +17,11 @@
                     (recur (- n 1) (max max-id u v) (conj! edges [u v])))))
         D (int-array (+ 1 M))]
     (doseq [[u v] E]
-      (aset D u (+ 1 (aget D u)))
-      (aset D v (+ 1 (aget D v))))
-    (->Graph E D)))
+      (aset-int D u (+ 1 (aget D u)))
+      (aset-int D v (+ 1 (aget D v))))
+    (->Graph E D (int-array (+ 1 M)))))
 
-(defn- filter-2 ^Graph [^Graph {D :degrees :as G}]
+(defn- filter-2 ^Graph [^Graph {D :degrees N :notes :as G}]
   (let [[E V] (loop [E-graph (:edges G) E-internal (transient []) verticies (transient [])]
                 (if (empty? E-graph)
                   [(persistent! E-internal) (persistent! verticies)]
@@ -32,14 +30,20 @@
                         dv (aget D v)]
                     (if (and (< 1 du) (< 1 dv))
                       (recur (next E-graph) (conj! E-internal e) verticies)
-                      (recur (next E-graph) E-internal (conj! verticies (if (= 1 du) v u)))))))]
+                      (let [w (if (= 1 du) v u)
+                            nw (aset-int N w (+ 1 (aget N w)))]
+                        (recur (next E-graph)
+                               E-internal
+                               (if (= 1 nw) (conj! verticies w) verticies)))))))]
     (doseq [v V]
-      (aset D v (- (aget D v) 1)))
+      (aset-int D v (- (aget D v) (aget N v)))
+      (aset-int N v 0))
     (assoc G :edges E)))
 
 (defn -main [& args]
-  (let [init-G (load-graph-2)]
+  (let [init-G (dump-times (load-graph-2))]
     (loop [G init-G steps 0]
+      (dump steps)
       (if (not (empty? (:edges G)))
-        (recur (filter-2 G) (+ 1 steps))
+        (recur (dump-times (filter-2 G)) (+ 1 steps))
         (println steps)))))
