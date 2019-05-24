7a2a0685b1d3f37b78d87a4df69e606909d9b668
M.Bakhterev <mob@k.imm.uran.ru>
2019-05-25 03:21:49 +0500

    5: Заметки натуралиста

--- haskell/zeroes.hs
+++ haskell/zeroes.hs
@@ -30,15 +30,26 @@ maxfactor n = loop (1, Factor n) (Factor 2) (Product n) where
                             then (k, p)
                             else loop (k, p) (bumpfactor p) (Product q)
 
+factors :: Integer -> [(Integer, Factor)]
+factors n = loop [] (Factor 2) (Product n) where
+  loop f p m = if overfactor p m
+               then f
+               else let (k, q) = factorout p m
+                    in if k == 0
+                       then loop f (bumpfactor p) m
+                       else loop ((k, p) : f) (bumpfactor p) q
+
 countfactors :: Factor -> Integer -> Integer
 countfactors (Factor p) n = if n == 0
                             then 0
                             else let q = quot n p
                                  in q + countfactors (Factor p) q
 
-showbn b n = "b: " ++ show b ++ " n: " ++ show n
+traceargs b n = let bi = toInteger b
+                    ni = toInteger n
+                in trace ("b: " ++ show bi ++ " n: " ++ show ni) bi
 
 zeroes :: Integral a => a -> a -> a
-zeroes b n = let (k, Factor p) = maxfactor (toInteger (trace (showbn (toInteger b)  (toInteger n)) b))
-                 m = countfactors (Factor p) (toInteger n)
-             in fromInteger (quot m k)
+zeroes b n = let fs = factors (traceargs b n)
+                 ks = map (\f -> countfactors (snd f) (toInteger n)) fs
+             in fromIntegral (minimum [quot t k | (t, k) <- zip ks (map fst fs)])
