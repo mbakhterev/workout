727e0287e776ec9391fd7a1e6a203797225e9c0a
mbakhterev <mob@k.imm.uran.ru>
2019-05-25 22:23:35 +0500

    5: Mix и записки натуралиста по мотивам

--- racket/mix.rkt
+++ racket/mix.rkt
@@ -1,7 +1,3 @@
-(define (freq<? p q) (if (= (car p) (car q))
-                         (char<? (cdr p) (cdr q))
-                         (> (car p) (car q))))
-
 (define (extract str)
   (filter (lambda (p) (not (= 1 (car p))))
           (map (lambda (l) (cons (length l) (car l)))
@@ -11,33 +7,34 @@
                                        (string->list str))
                                char<?)))))
 
-(define (item<? p q) (if (not (= (cadr p) (cadr q)))
-                         (> (cadr p) (cadr q))
-                         (if (and (zero? (car p)) (zero? (car q)))
-                             (char<? (cddr p) (cddr q))
-                             (zero? (car q)))))
-
-
-(define (encode n p)
-  (string-append (if (zero? n) "=" (number->string n))
-                 ":"
-                 (list->string (build-list (car p) (const (cdr p))))))
-
 (define (merge ps qs)
   (cond ((and (empty? ps) (empty? qs)) '())
+
         ((empty? qs) (cons (cons 1 (car ps)) (merge (cdr ps) '())))
         ((empty? ps) (cons (cons 2 (car qs)) (merge '() (cdr qs))))
 
         ((char=? (cdar ps) (cdar qs))
-         (cons (cond ((= (caar ps) (caar qs)) (cons 0 (car ps)))
+         (cons (cond ((= (caar ps) (caar qs)) (cons 3 (car ps)))
                      ((> (caar ps) (caar qs)) (cons 1 (car ps)))
                      (else (cons 2 (car qs))))
                (merge (cdr ps) (cdr qs))))
 
-        ((freq<? (car ps) (car qs)) (cons (cons 1 (car ps)) (merge (cdr ps) qs)))
+        ((char<? (cdar ps) (cdar qs)) (cons (cons 1 (car ps)) (merge (cdr ps) qs)))
         (else (cons (cons 2 (car qs)) (merge ps (cdr qs))))))
 
+(define (item<? p q) (if (not (= (cadr p) (cadr q)))
+                         (> (cadr p) (cadr q))
+                         (if (not (= (car p) (car q))) 
+                             (< (car p) (car q))
+                             (char<? (cddr p) (cddr q)))))
+
+(define (encode i)
+  (let ((n (car i))
+        (p (cdr i)))
+    (string-append (if (= 3 n) "=" (number->string n))
+                   ":"
+                   (list->string (build-list (car p) (const (cdr p)))))))
+
 (define (mix s t)
-  (string-join (map (lambda (x) (encode (car x) (cdr x)))
-                    (sort (merge (extract s) (extract t)) item<?))
+  (string-join (map encode (sort (merge (extract s) (extract t)) item<?))
                "/"))
