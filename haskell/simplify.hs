fe766406c8da15d30b9086489525a19947000e02
mbakhterev <mob@k.imm.uran.ru>
2019-06-05 20:58:24 +0500

    4: wip: simplify 8.1

--- haskell/simplify.hs
+++ haskell/simplify.hs
@@ -4,6 +4,7 @@ import Data.Char
 
 newtype Parser a = Parser { current :: StateT String Maybe a }
 parse = runStateT . current
+wrap = Parser . StateT
 
 instance Functor Parser where
   fmap f p = Parser (fmap f (current p))
@@ -17,8 +18,21 @@ instance Alternative Parser where
   p <|> q = Parser (current p <|> current q)
 
 instance Monad Parser where
-  m >>= f = (Parser . StateT) (\s -> do (v, t) <- parse m s
-                                        parse (f v) t)
+  m >>= f = (Parser . StateT) (\s -> do (v, t) <- parse m s; parse (f v) t)
+
+match :: Parser a -> (a -> Bool) -> Parser a
+match m p = m >>= \t -> if p t then return t else empty
+
+item :: Parser Char
+item = wrap (\s -> case s of [] -> empty; (c:cs) -> return (c,cs))
+
+space :: Parser ()
+space = many (match item isSpace) >>= \t -> return ()
+
+tokenize :: Parser a -> Parser a
+tokenize p = p <* space
+
+
 
 simplify :: [String] -> String -> String
 simplify es s = ""
