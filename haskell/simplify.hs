6d37308dc8d9cf295cd0718cd9eae125ca1d5308
mbakhterev <mob@k.imm.uran.ru>
2019-06-06 12:52:12 +0500

    4: wip: simplify

--- haskell/simplify.hs
+++ haskell/simplify.hs
@@ -18,7 +18,11 @@ instance Alternative Parser where
   p <|> q = Parser (current p <|> current q)
 
 instance Monad Parser where
-  m >>= f = (Parser . StateT) (\s -> do (v, t) <- parse m s; parse (f v) t)
+  m >>= f = wrap (\s -> do (v, t) <- parse m s; parse (f v) t)
+
+data Mono = Mono Integer (Maybe Char)
+          | Factor Integer [Mono]
+          deriving (Show)
 
 match :: Parser a -> (a -> Bool) -> Parser a
 match m p = m >>= \t -> if p t then return t else empty
@@ -32,7 +36,29 @@ space = many (match item isSpace) >>= \t -> return ()
 tokenize :: Parser a -> Parser a
 tokenize p = p <* space
 
+number :: Parser Integer
+number = tokenize (some (match item isDigit)) >>= \t -> return (read t :: Integer)
 
+variable :: Parser (Maybe Char)
+variable = tokenize ((match item isAlpha) >>= Just 
+
+sign :: Parser Integer
+sign = do s <- tokenize (match item (\c -> (c == '+') || (c == '-')))
+          case s of '+' -> 1; '-' -> -1
+
+mono :: Parser Mono
+mono = do s <- sign <|> pure 1
+          n <- number <|> pure 1
+          do v <- variable <|> ; 
+            
+
+poly :: Parser [Mono]
+
+-- E  -> T E'
+-- E' -> + T E' | -TE' |epsilon
+-- T  -> F T'
+-- T' -> * F T' | /FT' |epsilon
+-- F  -> (E) | int
 
 simplify :: [String] -> String -> String
 simplify es s = ""
