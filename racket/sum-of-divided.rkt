9f4470a11eabb5dabdd1aa23950f8e7f4a7994c3
mbakhterev <mob@k.imm.uran.ru>
2019-06-26 00:03:18 +0500

    4: sum-of-divided решена

--- racket/sum-of-divided.rkt
+++ racket/sum-of-divided.rkt
@@ -1,28 +1,45 @@
-(define (prime-out p N)
+(define (prime-out-one p N)
   (if (= 1 (abs p))
-      1
+      N
       (let-values (((q r) (quotient/remainder N p)))
-        (if (zero? r) (prime-out p q) N))))
+        (if (zero? r) (prime-out-one p q) N))))
 
-; Тут идея должна быть в том, что перебор следует начинать с максимального, уже
-; известно простого числа в P. По идее, если их просто cons-ить, то так и будет.
-; Но это не сработает, потому что могут быть пропуски в делителях. Ну, надо
-; убрать все известные делители, и начать перебор от 2.
+; P -- список пар (простое . сумма) 
+(define (primes-out N P)
+  (let ((result (foldl (lambda (p R)
+                         (let ((prime (car p))
+                               (sum (cdr p))
+                               (n (car R))
+                               (l (cdr R)))
+                           (let ((r (prime-out-one prime n)))
+                             (if (= r n)
+                                 ; prime не является делителем N
+                                 (cons n (cons p l))
+                                 ; prime является делителем, N нужно добавить в
+                                 ; сумму
+                                 (cons r (cons (cons prime (+ sum N)) l))))))
+                      (cons N '())
+                      P)))
+    (values (car result) (cdr result))))
 
 (define (factorize N P)
-  (let loop ((r (foldl prime-out (abs N) P))
-             (p 2)
-             (R P))
-    (cond  
-      ; Вытащили всё, что можно
-      ((= 1 (abs r)) R)
-      ; Что осталось, то простое число, если по модулю не равно 1.
-      ((> p r) (cons p R))
-      (else (let ((r-next (prime-out p r)))
-              (if (= r r-next)
-                  ; Если не поделилось, то p и не делитель
-                  (loop r (+ 1 p) R)
-                  ; Если поделилось, то p простой делитель
-                  (loop r-next (+ 1 p) (cons p R)))))))) 
+  (let-values (((n prime-pairs) (primes-out N P)))
+    (let loop ((r (abs n))
+               (p 2)
+               (R prime-pairs))
+      (cond  
+        ; Вытащили всё, что можно
+        ((= 1 (abs r)) R)
+        ; Что осталось, то простое число, и его надо добавить в список сумм
+        ((> p r) (cons (cons p N) R))
+        (else (let ((r-next (prime-out-one p r)))
+                (if (= r r-next)
+                    ; Если не поделилось, то p и не делитель
+                    (loop r (+ 1 p) R)
+                    ; Если поделилось, то p простой делитель
+                    (loop r-next (+ 1 p) (cons (cons p N) R))))))))) 
 
-(define (sum-of-divided lst) #f)
+(define (sum-of-divided lst)
+  (map (lambda (p) (list (car p) (cdr p)))
+       (sort (foldl factorize '() lst)
+             (lambda (a b) (< (car a) (car b))))))
