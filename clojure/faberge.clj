eabb7d0901774aaba05f52f3afd393821d0739ea
mbakhterev <mob@k.imm.uran.ru>
2019-06-29 19:28:51 +0500

    4: wip: faberge: ещё немного быстрее

--- clojure/faberge.clj
+++ clojure/faberge.clj
@@ -5,15 +5,23 @@
 ; же набора попыток и количества яиц n+1. Массивы удобно сделать длиной m+1.
 ; Функция работает при условии, что n < m
 
-(defn- expt-2 [n] (.subtract (.shiftLeft BigInteger/ONE n) BigInteger/ONE))
+; (defn- expt-2 [n] (.subtract (.shiftLeft BigInteger/ONE n) BigInteger/ONE))
 (defn- add-1 [p q] (.add p (.add q BigInteger/ONE)))
 
-(defn- big-zeroes [m]
-  (let [A (make-array BigInteger (+ 1 m))]
-    (doseq [i (range (+ 1 m))] (aset A i BigInteger/ZERO))
+(defn- expt-2 [n]
+  (loop [r 1N m 2N k n]
+    (cond (zero? k) (- r 1N)
+          (zero? (bit-and k 1)) (recur r (* m m) (bit-shift-right k 1))
+          :else (recur (* r m) (* m m) (bit-shift-right k 1)))))
+
+(defn- add-1 [p q] (+ 1N p q))
+
+(defn- zeroes [m]
+  (let [A (make-array clojure.lang.BigInt (+ 1 m))]
+    (doseq [i (range (+ 1 m))] (aset A i 0N))
     A))
 
-(defn step [n H]
+(defn step-old [n H]
   (assert (< (+ 1 n) (count H)))
 
   (loop [p (expt-2 n)
@@ -24,29 +32,8 @@
       H
       (recur (add-1 p q) (aget H (+ 1 i)) (+ 1 i)))))
 
-(defn step-old [n H HN]
-  (assert (< (+ 1 n) (count H)))
-  
-  (aset HN n (biginteger (- (.pow (biginteger 2) n) 1)))
-
-  (doseq [m (range (+ 1 n) (count H))]
-    (aset HN m (biginteger (+ 1
-                              (aget H (- m 1))
-                              (aget HN (- m 1)))))))
-
 (defn height-old [n m]
-  (cond ; (>= n m) (- (.pow (biginteger 2) m) 1)
-        :else (loop [i 0
-                     H (big-zeroes m)
-                     HN (big-zeroes m)]
-                ; (println i (vec H))
-                (if (= i n)
-                  (aget H m)
-                  (do (step-old i H HN)
-                      (recur (+ 1 i) HN H))))))
-
-(defn height [n m]
-  (loop [i 0 H (big-zeroes m)]
+  (loop [i 0 H (zeroes m)]
     (if (= i n)
       (aget H m)
       (recur (+ 1 i) (step i H)))))
