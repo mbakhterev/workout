f3aa35124ef06cf53b84644a2b57c9d463d62e20
M.Bakhterev <mob@k.imm.uran.ru>
2019-07-01 00:07:45 +0500

    4: done: faberge

--- racket/decompose.rkt
+++ racket/decompose.rkt
@@ -2,15 +2,16 @@
   (let ((l (integer-sqrt (- N (* k k))))) (min (- k 1) l)))
 
 (define (search N k)
-  (let ((R (- N (* k k))))
-    (if (zero? R)
-      (list k)
-      (let loop ((l (next-limit N k)))
-        (if (<= l 0) 
-            #f
-            (let ((p (search R l)))
+  (if (<= k 0)
+      #f
+      (let ((R (- N (* k k))))
+        (if (zero? R)
+            (list k)
+            (let ((p (search R (next-limit N k))))
               (if (list? p)
                   (cons k p)
-                  (loop (- l 1)))))))))
+                  (search N (- k 1))))))))
 
-(define (decompose n) #f)
+(define (decompose n)
+  (let ((r (search (* n n) (- n 1))))
+    (if (list? r) (reverse r) '())))
