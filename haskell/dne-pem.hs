a5c47ef88b89aafc0379364cbcec88a32fcb7b7d
mbakhterev <mob@k.imm.uran.ru>
2019-07-02 19:08:51 +0500

    4: wip: dne ↔ pme: dne ← pme доказано. В другую сторону тяжко

--- haskell/dne-pem.hs
+++ haskell/dne-pem.hs
@@ -5,8 +5,33 @@ import Data.Void
 type AxiomPEM = forall a. forall b. (a -> b) -> ((a -> Void) -> b) -> b
 type AxiomDNE = forall a. ((a -> Void) -> Void) -> a
 
+h :: a -> ((a -> Void) -> Void) -> a
+h x y = x
+
+g :: (a -> Void) -> ((a -> Void) -> Void) -> a
+g x y = absurd (y x)
+
+
 from :: AxiomDNE -> AxiomPEM
-from dne = undefined
+from dne = undefined -- \f g -> f id
 
 to :: AxiomPEM -> AxiomDNE
-to pem = pem (flip absurd)
+to pem = pem h g
+
+dne :: AxiomDNE
+dne = undefined
+
+pem :: AxiomPEM
+pem = undefined
+
+data A = A
+data B = B
+
+p :: A -> B
+p = undefined
+
+q :: (A -> Void) -> B
+q = undefined
+
+-- r :: AxiomDNE -> (((a -> Void) -> Void) -> Void) -> Void
+-- r dne f = f dne
